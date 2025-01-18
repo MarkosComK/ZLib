81a7c7d96c6696e12b7a4a897d22cdd33da584f5
Markos Santos <msantos@devexperts.com>
2025-01-18 17:12:29 +0000

    Update: new function done

--- src/char/char.zig
+++ src/char/char.zig
@@ -2,3 +2,4 @@ pub const isAlpha = @import("isAlpha.zig").isAlpha;
 pub const isDigit = @import("isDigit.zig").isDigit;
 pub const isAlNum = @import("isAlNum.zig").isAlNum;
 pub const isAscii = @import("isAscii.zig").isAscii;
+pub const isPrint = @import("isPrint.zig").isPrint;
---
+++ src/char/isPrint.zig
@@ -0,0 +1,3 @@
+pub fn isPrint(c: u8) bool {
+    return (c >= 32 and c <= 126);
+}
