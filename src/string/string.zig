6d99f8761f69a6e7cd50ca5a9e61d59c634edf1f
Markos Santos <msantos@devexperts.com>
2025-01-18 20:43:37 +0000

    Update: new function strCmp done

--- src/string/string.zig
+++ src/string/string.zig
@@ -5,3 +5,4 @@ pub const char = @import("../char/char.zig");
 pub const strLen = @import("strLen.zig").strLen;
 pub const strCpy = @import("strCpy.zig").strCpy;
 pub const strCat = @import("strCat.zig").strCat;
+pub const strCmp = @import("strCmp.zig").strCmp;
