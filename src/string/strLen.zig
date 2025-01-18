5493dc58b93285f0e56582c9f7137fee2b92287b
Markos Santos <msantos@devexperts.com>
2025-01-18 18:28:15 +0000

    Fix: out of bounds with const example: []u8 = undefined

--- src/string/strLen.zig
+++ src/string/strLen.zig
@@ -1,8 +1,11 @@
+const zlib = @import("string.zig").char;
+
 pub fn strLen(str: []const u8) u8 {
     var len: u8 = 0;
 
-    for (str) |_| {
-        len += 1;
+    for (str) |char| {
+        if (zlib.isPrint(char))
+            len += 1;
     }
     return (len);
 }
