9dee6d9186d1377f71d1e8957998e0bb98c28946
Markos Santos <msantos@devexperts.com>
2025-01-18 18:29:16 +0000

    Add: More tests

--- main.zig
+++ main.zig
@@ -2,6 +2,7 @@ const zlib = @import("zlib.zig");
 const std = @import("std");
 
 pub fn main() void {
+    // Character function tests
     const testChars = [_]u8{ 'A', 'z', '5', '!', ' ', 127, 128, 'M', 'q' };
 
     for (testChars) |char| {
@@ -15,4 +16,32 @@ pub fn main() void {
         std.debug.print("toLower: {c}\n", .{zlib.char.toLower(char)});
         std.debug.print("\n", .{});
     }
+
+    // String operation tests
+    const str1 = "Hello";
+    const str2 = " World!";
+    var buffer: [20]u8 = undefined;
+
+    std.debug.print("String Operations Tests:\n", .{});
+    std.debug.print("strLen('{s}'): {}\n", .{ str1, zlib.str.strLen(str1) });
+
+    _ = zlib.str.strCpy(&buffer, str1);
+    std.debug.print("strCpy result: '{s}'\n", .{buffer});
+
+    _ = zlib.str.strCat(&buffer, str2);
+    std.debug.print("strCat result: '{s}'\n", .{buffer});
+
+    //std.debug.print("strCmp('Hello', 'Hello'): {}\n", .{zlib.str.strCmp("Hello", "Hello")});
+    //std.debug.print("strCmp('Hello', 'World'): {}\n", .{zlib.str.strCmp("Hello", "World")});
+
+    //const char_to_find = 'o';
+    //const char_pos = zlib.str.strChr(str1, char_to_find);
+    //std.debug.print("strChr('{s}', '{c}'): {?}\n", .{ str1, char_to_find, char_pos });
+
+    //const char_pos_r = zlib.str.strRChr(str1, char_to_find);
+    //std.debug.print("strRChr('{s}', '{c}'): {?}\n", .{ str1, char_to_find, char_pos_r });
+
+    //const substr = "lo";
+    //const substr_pos = zlib.str.strStr(str1, substr);
+    //std.debug.print("strStr('{s}', '{s}'): {?}\n", .{ str1, substr, substr_pos });
 }
