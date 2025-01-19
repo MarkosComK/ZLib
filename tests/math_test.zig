1075ca7920053c78658df98839a7e7fb17436896
Markos Santos <msantos@devexperts.com>
2025-01-19 20:59:58 +0000

    Add: better formating

--- tests/math_test.zig
+++ tests/math_test.zig
@@ -12,7 +12,7 @@ const Color = struct {
 
 fn printTestResult(comptime format: []const u8, test_name: []const u8, expected: anytype, actual: anytype, passed: bool) void {
     const status = if (passed) Color.GREEN ++ "PASS" ++ Color.RESET else Color.RED ++ "FAIL" ++ Color.RESET;
-    std.debug.print("{s:<12} | {s:<12} | Expected: " ++ format ++ " | Actual: " ++ format ++ " | {s}\n", .{ test_name, "", expected, actual, status });
+    std.debug.print("{s:^12} | {s:<0} | Expected: | " ++ format ++ " | Actual: | " ++ format ++ " | {s}\n", .{ test_name, "", expected, actual, status });
 }
 
 test "Math Function Tests" {
@@ -23,9 +23,9 @@ test "Math Function Tests" {
     const pow_test2 = zib.math.pow(5, 0);
     const pow_test3 = zib.math.pow(-2, 3);
 
-    printTestResult("{d:<2}", "pow(2, 3)", 8, pow_test1, pow_test1 == 8);
-    printTestResult("{d:<2}", "pow(5, 0)", 1, pow_test2, pow_test2 == 1);
-    printTestResult("{d:<2}", "pow(-2, 3)", -8, pow_test3, pow_test3 == -8);
+    printTestResult("{d:^5}", "pow(2, 3)", 8, pow_test1, pow_test1 == 8);
+    printTestResult("{d:^5}", "pow(5, 0)", 1, pow_test2, pow_test2 == 1);
+    printTestResult("{d:^5}", "pow(-2, 3)", -8, pow_test3, pow_test3 == -8);
 
     try testing.expectEqual(pow_test1, 8);
     try testing.expectEqual(pow_test2, 1);
@@ -35,8 +35,8 @@ test "Math Function Tests" {
     const sqrt_test1 = zib.math.sqrt(16);
     const sqrt_test2 = zib.math.sqrt(0);
 
-    printTestResult("{d:<2}", "sqrt(16)", 4, sqrt_test1, sqrt_test1 == 4);
-    printTestResult("{d:<2}", "sqrt(0)", 0, sqrt_test2, sqrt_test2 == 0);
+    printTestResult("{d:^5}", "sqrt(16)", 4, sqrt_test1, sqrt_test1 == 4);
+    printTestResult("{d:^5}", "sqrt(0)", 0, sqrt_test2, sqrt_test2 == 0);
 
     try testing.expectEqual(sqrt_test1, 4);
     try testing.expectEqual(sqrt_test2, 0);
@@ -45,8 +45,8 @@ test "Math Function Tests" {
     const abs_test1 = zib.math.abs(-42);
     const abs_test2 = zib.math.abs(42);
 
-    printTestResult("{d:<2}", "abs(-42)", 42, abs_test1, abs_test1 == 42);
-    printTestResult("{d:<2}", "abs(42)", 42, abs_test2, abs_test2 == 42);
+    printTestResult("{d:^5}", "abs(-42)", 42, abs_test1, abs_test1 == 42);
+    printTestResult("{d:^5}", "abs(42)", 42, abs_test2, abs_test2 == 42);
 
     try testing.expectEqual(abs_test1, 42);
     try testing.expectEqual(abs_test2, 42);
