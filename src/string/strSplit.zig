ae931d603a28cae28c9d000ee33fd2de8574382e
Markos Santos <msantos@devexperts.com>
2025-01-20 21:56:29 +0000

    CountWords: test to do

---
+++ src/string/strSplit.zig
@@ -0,0 +1,54 @@
+
+//==============================================================================
+// |‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|
+// |                                                                           |
+// | ███████╗██╗██████╗  ────────── ZIB LIBRARY ────────                       |
+// | ╚══███╔╝██║██╔══██╗                                                       |
+// |   ███╔╝ ██║██████╔╝  Created: 2025-01-20 20:41:10                         |
+// |  ███╔╝  ██║██╔══██╗  Last Updated: 2025-01-20 20:20:19                    |
+// | ███████╗██║██████╔╝                                                       |
+// | ╚══════╝╚═╝╚═════╝                                                        |
+// |___________________________________________________________________________|
+//==============================================================================
+
+const strLen = @import("strLen.zig").strLen;
+
+//first we need to count words using delimiter
+//remember that we can have spaces before and after string so jump them
+//
+//"--this-----is-a-split--example-"
+fn countWords(str: []const u8, delimiter: u8) usize {
+    var words: usize = 0;
+    var in_word: bool = false;
+
+    for (str) |char| {
+        if (char == delimiter) {
+            in_word = false;
+        } else if (!in_word) {
+            words += 1;
+            in_word = true;
+        }
+    }
+    return words;
+}
+
+pub fn strSplit(allocator: std.mem.Allocator, str: []const u8, delimiter: u8) ![][]const u8 {
+    if (strLen(str) == 0) return null;
+
+    const words: usize = countWords(str, delimiter);
+    var split: [][]u8 = allocator.alloc([]u8, words);
+
+}
+
+const std = struct {
+    mem {
+        allocator {
+            c_malloc{
+
+            }
+            gpa{
+
+            }
+        }
+    }
+}
